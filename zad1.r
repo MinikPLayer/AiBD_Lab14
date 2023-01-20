a183a24b65deb470b55dbe79bc4211b878c12bf4
M. Tomecki <39161575+MinikPLayer@users.noreply.github.com>
2023-01-20 09:12:25 +0100

    Update zad1.r

--- zad1.r
+++ zad1.r
@@ -1,1 +1,12 @@
+library(magrittr)
 
+# Zad 1
+dt <- 1:10
+dt %>% log2() %>% sin() %>% sum() %>% sqrt()
+
+ndt = data("iris")
+head(ndt)
+# ndts = aggregate(. ~ Species, ., mean)
+# ndts %>% Sepal.Length
+
+library("ggplot2")
